49c27e62f40ecf6c7ee8e8cefcfaf33454d64e12
miu <dezabaladjay@gmail.com>
2026-08-06 11:26:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-//config
-const MQTT_CONFIG = {
-    MQTT_BROKER: 'wss://8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt',
-    MQTT_USER: 'ULRC',
-    MQTT_PASSWORD: 'unlisabaw'
-}; 
+const CONFIG = {
+  MQTT_BROKER: 'wss://your_cluster_url_here:8884/mqtt',
+  MQTT_USER: 'your_username',
+  MQTT_PASSWORD: 'your_password'
+};
