d74e81a588c0c574729dfdc4da15e0f86e2457eb
miu <dezabaladjay@gmail.com>
2026-08-06 11:46:51 +0800

    Refactor MQTT configuration object structure

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const CONFIG = {
-  MQTT_BROKER: 'wss://your_cluster_url_here:8884/mqtt',
-  MQTT_USER: 'your_username',
-  MQTT_PASSWORD: 'your_password'
+const MQTT_CONFIG = {
+    broker: "BROKER_PLACEHOLDER",
+    username: "USERNAME_PLACEHOLDER",
+    password: "PASSWORD_PLACEHOLDER"
 };
