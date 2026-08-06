879df32022f7207b912d282d6ad0f567f89a4679
miu <dezabaladjay@gmail.com>
2026-08-06 11:48:37 +0800

    Update MQTT configuration with new credentials

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const MQTT_CONFIG = {
-    broker: "BROKER_PLACEHOLDER",
-    username: "USERNAME_PLACEHOLDER",
-    password: "PASSWORD_PLACEHOLDER"
+  MQTT_BROKER: 'wss://8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt',
+  MQTT_USER: 'ULRC',
+  MQTT_PASSWORD: 'unlisabaw'
 };
