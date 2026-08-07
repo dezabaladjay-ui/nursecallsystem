6ef32c28ac0114759fa164121590cb048d02d92c
miu <dezabaladjay@gmail.com>
2026-08-07 11:50:58 +0800

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const MQTT_CONFIG = {
-  MQTT_BROKER: "wss://8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt",
+  MQTT_BROKER: "8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt",
   MQTT_USERNAME: "ULRC",
   MQTT_PASSWORD: "unlisabaw",
   clientId: "WebDashboard_" + Math.random().toString(16).substring(2, 8)
