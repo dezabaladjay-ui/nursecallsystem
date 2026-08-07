55f347703a4a0135f18e3f25bbfaceee6b17e5ab
miu <dezabaladjay@gmail.com>
2026-08-07 14:52:41 +0800

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,10 +1,12 @@
+// 1. Determine configuration safely
 const activeConfig = typeof MQTT_CONFIG !== 'undefined' ? MQTT_CONFIG : {
-    broker: "8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt", // Public broker test URL
+    broker: "wss://test.mosquitto.org:8081/mqtt",
     username: "",
     password: "",
     clientId: "PublicDashboard_" + Math.random().toString(16).substring(2, 8)
 };
 
+// 2. Connect once (do not redeclare 'client' anywhere else)
 const client = mqtt.connect(activeConfig.broker, {
     username: activeConfig.username,
     password: activeConfig.password,
