4f7a115a06daa67156aa913b4e058fcd5fe66b13
miu <dezabaladjay@gmail.com>
2026-08-07 11:10:22 +0800

    Refactor MQTT configuration structure in config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-const CONFIG = {
-  MQTT_BROKER: 'wss://8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt',
-  MQTT_USER: 'ULRC',
-  MQTT_PASSWORD: 'unlisabaw'
+const MQTT_CONFIG = {
+  broker: "8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud",
+  port: 8884, // WebSocket secure port
+  username: "ULRC",
+  password: "unlisabaw",
+  clientId: "WebDashboard_" + Math.random().toString(16).substring(2, 8)
 };
