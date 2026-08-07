a3f332abcebf2fd6e75663c27894e2da42663748
miu <dezabaladjay@gmail.com>
2026-08-07 11:35:35 +0800

    Refactor MQTT_CONFIG for new broker URL
    Updated MQTT configuration to use new broker URL format.

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const MQTT_CONFIG = {
-  broker: "8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud",
-  port: 8884, 
-  username: "ULRC",
-  password: "unlisabaw",
+  MQTT_BROKER: "wss://8489829bb9624d2b82642b5272686b5c.s1.eu.hivemq.cloud:8884/mqtt",
+  MQTT_USERNAME: "ULRC",
+  MQTT_PASSWORD: "unlisabaw",
   clientId: "WebDashboard_" + Math.random().toString(16).substring(2, 8)
 };
